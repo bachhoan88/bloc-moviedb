fe65ba59224fd5e6911b95b42240abb2f93bccec
Bach Ngoc Hoai <bach.ngoc.hoai@sun-asterisk.com>
2021-01-13 18:50:01 +0700

    Update UT

--- test/src/data/repository/movie_repository_impl_test.dart
+++ test/src/data/repository/movie_repository_impl_test.dart
@@ -11,7 +11,7 @@ class ClientMock extends Mock implements Client {}
 
 void main() {
   group('Fetch Movies with Type', () {
-    test('Return a [Movie] when fetch Movie with Type success', () async {
+    test('Return a [Movie] when fetch Movie with Type [popular]', () async {
       final client = ClientMock();
       final type = Constant.popular;
       final API_KEY = 'd61431a2fb64b6e56c6f086952e63ab6';
@@ -24,6 +24,42 @@ void main() {
       final repository = MovieRepositoryImpl(client: client);
 
       expect(await repository.fetchMovies(type), isA<List<Movie>>());
+    });
+
+    test('Throws [Exception] on non-200 response', () async {
+      final client = ClientMock();
+      final type = Constant.popular;
+      final API_KEY = 'd61431a2fb64b6e56c6f086952e63ab6';
+
+      when(client.get('https://api.themoviedb.org/3/movie/$type?api_key=$API_KEY')).thenAnswer((_) async => Response(
+        'Not Found',
+        404,
+      ));
+
+      final repository = MovieRepositoryImpl(client: client);
+
+      expect(
+            () async => await repository.fetchMovies(type),
+        throwsA(isA<Exception>()),
+      );
+    });
+
+    test('Throws [Exception] when fetch Movie with Type [popular] on parser error', () async {
+      final client = ClientMock();
+      final type = Constant.popular;
+      final API_KEY = 'd61431a2fb64b6e56c6f086952e63ab6';
+
+      when(client.get('https://api.themoviedb.org/3/movie/$type?api_key=$API_KEY')).thenAnswer((_) async => Response(
+        'error parser',
+        200,
+      ));
+
+      final repository = MovieRepositoryImpl(client: client);
+
+      expect(
+            () async => await repository.fetchMovies(type),
+        throwsA(isA<Exception>()),
+      );
     });
   });
 }
